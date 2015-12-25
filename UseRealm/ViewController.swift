1fb4865c94d89260fb442da9673fc5324fa72a45
kashitaka <makkausu@gmail.com>
2015-12-25 15:02:01 +0900

    Initial Commit

---
+++ UseRealm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UseRealm
+//
+//  Created by kashitaka on 2015/12/25.
+//  Copyright © 2015年 kashitaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
